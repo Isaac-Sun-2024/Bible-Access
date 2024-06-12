bb645dc55db083bea51d752812df29adf21de87d
Isaac-Sun <157567638+Isaac-Sun-2024@users.noreply.github.com>
2024-06-12 18:55:53 +1200

    Add files via upload

---
+++ install-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/Isaac-Sun-2024/Bible-Access/main/kjv.tsv
+curl https://raw.githubusercontent.com/Isaac-Sun-2024/Bible-Access/main/bible-is.c
+
+mkdir isaac
+
+mv kjv.tsv /usr/local/bin
+mv bible-is.c isaac
+gcc bible-is.c -o bible
+
+cd isaac/output
+mv bible /usr/local/bin
