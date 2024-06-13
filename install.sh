a9891e178ee20fb868c7253db50a524877f37676
Isaac-Sun <157567638+Isaac-Sun-2024@users.noreply.github.com>
2024-06-13 13:59:47 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,10 @@ sudo gcc bible-is.c -o bible-kjv
 
 sudo mv bible-kjv /usr/local/bin
 
-echo 'YOU ARE DONE. GOOD JOB. - Isaac'
+echo '(Black screen with text; The sound of buzzing bees can be heard)
+According to all known laws
+of aviation,
+there is no way a bee
+should be able to fly.
+Its wings are too small to get
+its fat little body off the ground. - Isaac'
